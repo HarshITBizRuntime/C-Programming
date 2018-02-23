ae50048bd6a2c475dc274598894b32b058c021c7
HarshITBizRuntime <harsh.r@bizruntime.net>
2018-02-22 17:44:25 -0800

    arrays and Generics Code

---
+++ ArraysGenerics.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Messaging;
+
+namespace Programming
+{
+    class ArraysGenerics
+    {
+
+        static void CreateOneDimArray()
+        {
+            int[] A = new int[5];  //Declare single dimensional array and accept 5 integer values from the user. 
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter the array Element:->");
+                A[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            PrintArray(A);
+            Array.Sort(A); //Then sort the input in ascending order and display output
+            PrintArray(A);
+        }
+        static void PrintArray(int [] A)
+        {
+             Console.WriteLine("Elements of array");
+            foreach(int i in A)
+            {
+                Console.Write(i + ",");
+            }
+        }
+        int[,] A = new int[3, 3];  //Declare two dimensional array and accept 3 * 3 integer values from the user. 
+        int[,] B = new int[3, 3];
+        public void CreateTwoDimArray()
+        {
+            int i, j;
+            for (i = 0; i <3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    Console.Write("Enter the 2D array Element:->");
+                    A[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            this.Copy2DArray();
+            Console.WriteLine("");
+            this.PrintTwoDimArry();
+        }
+        public void Copy2DArray()
+        {
+            this.PrintTwoDimArry();
+            Console.WriteLine("");
+            Console.WriteLine("copy A array to B Array");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    B[i, j] = A[i, j];
+                }
+            }
+        }
+        public void PrintTwoDimArry()
+        {
+            Console.WriteLine("print 2D Array");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(this.B[i, j]);
+                }
+            }
+        }
+
+        static void CommonFunciton<G>(G s)
+        {
+            Console.WriteLine("the Value of S is :->" + s);
+        }
+        class G3<G1, G2>
+        {
+            G1 test1;
+            G2 test2;
+
+           public G3(G1 test1, G2 test2)
+            {
+                Console.WriteLine(this.test1 = test1);
+                Console.WriteLine(this.test2 = test2);
+            }
+            public void display(G1 s1, G2 s2)
+            {
+                Console.WriteLine("the value s is:-> " + s1+ "," +  s2);
+            }
+        }
+        static void Main(string[] args)
+        {
+           // CreateOneDimArray();
+            ArraysGenerics ag = new ArraysGenerics();
+            //ag.CreateTwoDimArray();        
+            /* //   when the datatype is change  that time we are performing Generics at functional level
+                CommonFunciton<int>(100);
+                CommonFunciton<Double>(1.000);
+                CommonFunciton<Boolean>(true);
+                CommonFunciton<string>("100");
+                CommonFunciton<char>('B');*/
+            /* // when the number of argument is changing that time we are performing same as class Level
+                G3<int, int> g1 = new G3<int, int>();
+                G3<string, string> g2 = new G3<string, string>();
+                G3<double, double> g3 = new G3<double, double>();
+                g1.display(20,10);
+                g2.display("Hello","hie");
+                g3.display(20.45, 125.12);*/
+            G3<string, string> g2 = new G3<string, string>("abc","test");
+            G3<double, double> g3 = new G3<double, double>(1.0,2.0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Collections.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Programming
+{
+    class Collections
+    {
+            List<String> list = new List<String>();
+        public  void AddItemInList()
+        {
+            Console.WriteLine("List added:->");
+            list.Add("Hello");
+            list.Add("Hie");
+            list.Add("How are you");
+            
+            this.PrintItemInList();
+            Console.WriteLine();
+            this.PrintItemSortedInList();
+            Console.WriteLine();
+            this.RemoveItemInList();
+            Console.WriteLine();
+            this.IsertItemInSpecificListIndex();
+            Console.WriteLine();
+            this.PrintItemReverseInList();
+        }
+        public void PrintItemInList()
+        {
+            foreach (string  s in list)
+                Console.WriteLine(s.ToString() + ",");
+        }
+        public void PrintItemSortedInList()
+        {
+            list.Sort();
+            Console.WriteLine("After sorting :->");
+            foreach (string s in list)
+            Console.WriteLine(s.ToString() + ",");
+        }
+        public void PrintItemReverseInList()
+        {
+            list.Reverse();
+            Console.WriteLine("After Reverse :->");
+            foreach (string s in list)
+            Console.WriteLine(s.ToString() + ",");
+        }
+        public void IsertItemInSpecificListIndex()
+        {
+            list.Insert(2,"abc");
+            Console.WriteLine("After List added in specific position:->");
+            foreach (string s in list)
+            Console.WriteLine(s.ToString() + ",");
+        }
+        public void RemoveItemInList()
+        {
+            list.Remove("Hie");
+            Console.WriteLine("After removing the list :->");
+            foreach (string s in list)
+            Console.WriteLine(s.ToString() + ",");
+        }
+        static void Main(string[] args)
+        {
+            Collections c = new Collections();
+            c.AddItemInList();
+            Console.ReadKey();
+        }
+    }
+}
